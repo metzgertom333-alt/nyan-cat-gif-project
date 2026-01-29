c4aaf16f61c25089afe6076a2ecd05149325db6c
metzgertom333-alt <metzgertom333@gmail.com>
2026-01-29 15:35:55 +0100

    Add files via upload

---
+++ create_a_gif.py
@@ -0,0 +1,10 @@
+import imageio.v3 as iio
+
+
+filenames = ["nyan-cat1.png", "nyan-cat2.png", "nyan-cat3.png"]
+images = []
+
+for filename in filenames:
+    images.append(iio.imread(filename))
+
+iio.imwrite("nyan-cat.gif", images, duration=100, loop=0)
